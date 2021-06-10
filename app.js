21bbe07023120e5d10df3e07e887a359366604d2
tarzan114 <tanzirh10@gmail.com>
2021-06-10 16:40:11 -0400

    comments

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express=require('express');
 const app=express();
 const exphbs = require('express-handlebars');
 app.set('view engine', 'handlebars');
-
+//port config
 const PORT=process.env.PORT || 5000;
-//Sets handlebars configurations (we will go through them later on)
+//Sets handlebars configurations 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+//set app so it is able to use JSON data and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-
 app.use(express.static('public'));
-
+//sets entry point for app and also routing for whole app
 const routes=require('./routes/Routes');
 app.use('/',routes);
 
@@ -19,3 +19,12 @@ app.use('/',routes);
 // app.use(cors())
 
 app.listen(PORT,()=>console.log(`server running at port ${PORT}`));
+/*Thanks for considering me as a participant for the case study, below are some improvements i wanted to do:
+1. make a more user friendly UI and experience. Configure the CSS more, have it send alerts instead of redirecting you
+2.not sure if this counts but im still not sure if my method for fetching the API in the routes page was the best practice,i will have to study that more
+3.i wanted to implement the viewed counter for the pages using google analytics but didnt have time and it was kind of confusing to me
+
+all in all thank you for granting me this chance to showcase my skills
+Thanks, 
+Tanzir
+*/
--- routes/Routes.js
+++ routes/Routes.js
@@ -5,6 +5,7 @@ for the latest comic and when it does, it will redirect you to the most up to da
 will change to /:${most up to date comics num here}
 */
 Router.get('/',async(req,res)=>{
+    /*entry point for app, whenever someone enters the app it will get the latest comic and get the latest comic number and redirect the url to the /{latest comic num} route */
     const url_api='http://xkcd.com/info.0.json';
     try {
         await fetch(url_api)
@@ -21,6 +22,10 @@ Router.get('/',async(req,res)=>{
     
 });
 Router.get('/:num',async(req,res)=>{
+    /*the main routing for the whole app this will get the num url parameter and render the assosciated comic based on the url parameter
+    then it will put all necessary data into a  JSON object and finally render the handlebars main page with the obj JSON object
+    if there is an error (like if they enter a string for url parameters or they go and add a comic num that doesnt exist it will
+        restrict them from going to that url and redirect them back)*/
     const url_api=`http://xkcd.com/${req.params.num}/info.0.json`
     try {
         await fetch(url_api)
@@ -46,6 +51,8 @@ Router.get('/:num',async(req,res)=>{
     }
 })
 Router.post('/:num',async (req,res)=>{
+    /*post req handler it will first read the body of the button that submitted to the route page and conditionally redirect to the get route for /${num} and that 
+    redirect will redirect it based on the button clicked(ex:left button will make it go to the prev comic) */
     console.log(req.params.num)
     console.log(req.body.class)
     if (req.body.left){
